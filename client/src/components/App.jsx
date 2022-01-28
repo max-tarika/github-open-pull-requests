5567fb14c71be0006eaba12532177e6a12b0c5d0
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2022-01-28 16:43:25 -0700

    Frontend Template setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [url, setUrl] = useState('');
+
+  const handleRepoUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submit Clicked');
+  }
+
   return (
     <div>
-      Hello World
+      <h1>GitHub Repo Open Pull Requests</h1>
+      <form id="repo-url-form" onSubmit={handleSubmit}>
+        <label htmlFor="repo-url-input">
+          Enter Repo URL Here:
+          <input type="text" id="repo-url-input" onChange={handleRepoUrlChange} value={url} />
+        </label>
+        <button type="submit" id="repo-url-submit">Submit</button>
+      </form>
     </div>
   );
 }
