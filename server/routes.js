d003ea30c426b9268809352e1d7a1cd5134dfb20
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2022-01-29 12:39:59 -0700

    setup open pull requests route

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
+const controller = require('./controllers');
+
+router.get('/openPullRequests', controller.openPullRequests.get);
 
 module.exports = router;
