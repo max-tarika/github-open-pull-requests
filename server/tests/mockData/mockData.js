725a2ee7a21b47c441520c170afaf06574241f2c
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2022-02-01 11:30:53 -0700

    Added mock data for testing

---
+++ server/tests/mockData/mockData.js
@@ -0,0 +1,31 @@
+const testOnePullResponse = [
+  {
+    title: 'marriot',
+    commits_url: 'marriot',
+  },
+  {
+    title: 'ritz',
+    commits_url: 'ritz',
+  },
+];
+
+const marriotCommitsResponse = [0, 1, 2];
+
+const ritzCommitsResponse = [0, 1];
+
+const testTwoPullResponse = [
+  {
+    title: 'hilton',
+    commits_url: 'hilton',
+  },
+];
+
+const hiltonCommitsResponse = [];
+
+module.exports = {
+  testOnePullResponse,
+  marriotCommitsResponse,
+  ritzCommitsResponse,
+  testTwoPullResponse,
+  hiltonCommitsResponse,
+};
