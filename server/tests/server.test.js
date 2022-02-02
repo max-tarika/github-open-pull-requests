ea7cdb252f77671f8307ac1780efc0cc6276d3d7
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2022-02-02 10:06:37 -0700

    Added a few more assertions to tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -26,10 +26,10 @@ describe('Pull Request API Function', () => {
   });
 
   test('Should respond to GET requests for /pullReqs with pull request & commit data', async () => {
-    const expectedResponse = JSON.stringify([
+    const expectedResponse = [
       { pullRequestTitle: 'marriot', commits: 3 },
       { pullRequestTitle: 'ritz', commits: 2 },
-    ]);
+    ];
 
     mock.onGet('https://api.github.com/repos/testOne/testOneRepo/pulls').reply(200, testOnePullResponse);
     mock.onGet('marriot').reply(200, marriotCommitsResponse);
@@ -40,15 +40,17 @@ describe('Pull Request API Function', () => {
       .query({ url: 'https://github.com/testOne/testOneRepo' });
 
     expect(actualResponse.statusCode).toBe(200);
-    expect(actualResponse.text).toEqual(expectedResponse);
+    expect(actualResponse.type).toBe('application/json');
+    expect(Array.isArray(actualResponse.body)).toBe(true);
+    expect(actualResponse.body).toEqual(expectedResponse);
   });
 
   test('Should respond to GET requests with an invalid URL with a 400 status code', async () => {
-    const response = await request(app)
+    const res = await request(app)
       .get('/pullReqs')
       .query({ url: 'invalid url' });
 
-    expect(response.statusCode).toBe(400);
-    expect(response.text).toEqual('Invalid URL');
+    expect(res.statusCode).toBe(400);
+    expect(res.text).toEqual('Invalid URL');
   });
 });
